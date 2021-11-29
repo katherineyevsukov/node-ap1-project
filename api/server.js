5998faeb0a82f9fe4e97fc5d1943732e614c66aa
Kat <kyevsukov@gmail.com>
2021-11-29 15:45:57 -0500

    fixed get by id error messages

--- api/server.js
+++ api/server.js
@@ -22,9 +22,9 @@ server.get('/api/users', async (req, res) => {
 server.get('/api/users/:id', async (req, res) => {
     try {
         const user = await Users.findById(req.params.id)
-        res.status(200).json(user)
+        !user ? res.status(404).json({ message: "The user with the specified ID does not exist"}) : res.status(200).json(user)
     } catch(err) {
-        res.status(404).json({ message: "The user with the specified ID does not exist"})
+        res.status(500).json({ message: "The user information could not be retrieved" })
     }
 })
 module.exports = server; // EXPORT YOUR SERVER instead of {}
