4e7cbe1c87ee6732312e207a84b28b6cca9a4339
Kat <kyevsukov@gmail.com>
2021-11-29 18:18:22 -0500

    can delete a user

--- users-app/src/App.js
+++ users-app/src/App.js
@@ -5,26 +5,36 @@ import "./App.css";
 function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const res = await axios.get("http://localhost:9000/api/users/");
-        console.log(res);
-        setUsers(res.data);
-      } catch (err) {
-        console.log(err);
-      }
+  async function fetchUsers() {
+    try {
+      const res = await axios.get("http://localhost:9000/api/users/");
+      setUsers(res.data);
+    } catch (err) {
+      console.log(err);
     }
+  }
+
+  useEffect(() => {
     fetchUsers();
   }, []);
+
+  const deleteUser = async(id) => {
+    try{
+      const res = await axios.delete(`http://localhost:9000/api/users/${id}`)
+      setUsers(users.filter(us => us.id !== res.data.id))
+    } catch(err) {
+      console.log(err)
+    }
+  }
 
   return (
     <div className="App">
       <h1>Hello World</h1>
-      {users.map((us) => (
-        <div>
+      {users.map((us, idx) => (
+        <div key={idx}>
           <h2>{us.name}</h2>
           <h3>{us.bio}</h3>
+          <button onClick={() => {deleteUser(us.id)}}>Delete</button>
         </div>
       ))}
     </div>
