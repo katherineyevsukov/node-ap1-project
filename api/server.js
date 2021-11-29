a16a50025c72b0c5808beca2a8e728725d7133fb
Kat <kyevsukov@gmail.com>
2021-11-29 16:24:58 -0500

    finished MVP

--- api/server.js
+++ api/server.js
@@ -48,11 +48,9 @@ server.post("/api/users", (req, res) => {
         res.status(201).json(user);
       })
       .catch((err) => {
-        res
-          .status(500)
-          .message({
-            message: "There was an error while saving the user to the database",
-          });
+        res.status(500).message({
+          message: "There was an error while saving the user to the database",
+        });
       });
   }
 });
@@ -69,6 +67,31 @@ server.delete("/api/users/:id", (req, res) => {
     .catch((err) => {
       res.status(500).json({ message: "The user could not be removed" });
     });
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ message: "Please provide name and bio for the user" });
+  } else {
+    Users.update(req.params.id, { name, bio })
+      .then((updated) => {
+        !updated
+          ? res
+              .status(404)
+              .json({
+                message: "The user with the specified ID does not exist",
+              })
+          : res.status(200).json(updated);
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ message: "The user information could not be modified" });
+      });
+  }
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
