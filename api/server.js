0620d3adcd89a21d39589bb11da9caff2017d7a2
Kat <kyevsukov@gmail.com>
2021-11-29 15:42:47 -0500

    can get by id

--- api/server.js
+++ api/server.js
@@ -19,4 +19,12 @@ server.get('/api/users', async (req, res) => {
     }
 })
 
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const user = await Users.findById(req.params.id)
+        res.status(200).json(user)
+    } catch(err) {
+        res.status(404).json({ message: "The user with the specified ID does not exist"})
+    }
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
