4e827b60905e3fcc5499ef63b0161a6f96206560
Kat <kyevsukov@gmail.com>
2021-11-29 15:38:56 -0500

    can get all users

--- api/server.js
+++ api/server.js
@@ -10,4 +10,13 @@ server.get('/', (req, res) => {
     res.json({ message: 'api is running'})
 })
 
+server.get('/api/users', async (req, res) => {
+    try{
+        const users = await Users.find()
+        res.status(200).json(users)
+    }catch(err){
+        res.status(500).json({ message: "The users information could not be retrieved"})
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
