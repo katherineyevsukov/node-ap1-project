b87bfe50adff8a88c2019c7d618d4a6be8d9bd1a
Kat <kyevsukov@gmail.com>
2021-11-29 19:01:05 -0500

    can add new user

--- users-app/src/App.js
+++ users-app/src/App.js
@@ -2,8 +2,15 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+const initalFormValues = {
+  name: "",
+  bio: "",
+};
+
 function App() {
   const [users, setUsers] = useState([]);
+  const [editing, setEditing] = useState(false);
+  const [formValues, setFormValues] = useState(initalFormValues);
 
   async function fetchUsers() {
     try {
@@ -18,14 +25,35 @@ function App() {
     fetchUsers();
   }, []);
 
-  const deleteUser = async(id) => {
-    try{
-      const res = await axios.delete(`http://localhost:9000/api/users/${id}`)
-      setUsers(users.filter(us => us.id !== res.data.id))
-    } catch(err) {
+  const deleteUser = async (id) => {
+    try {
+      const res = await axios.delete(`http://localhost:9000/api/users/${id}`);
+      setUsers(users.filter((us) => us.id !== res.data.id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleChanges = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+    
+  }
+
+  const addUser = async (e) => {
+    e.preventDefault()
+    try {
+      const newUser = await axios.post('http://localhost:9000/api/users/', formValues)
+      setUsers([...users, newUser.data])
+      setFormValues(initalFormValues)
+    } catch (err) {
       console.log(err)
     }
   }
+
+  const handleEdit = async (id) => {};
 
   return (
     <div className="App">
@@ -34,9 +62,36 @@ function App() {
         <div key={idx}>
           <h2>{us.name}</h2>
           <h3>{us.bio}</h3>
-          <button onClick={() => {deleteUser(us.id)}}>Delete</button>
+          <button
+            onClick={() => {
+              deleteUser(us.id);
+            }}
+          >
+            Delete
+          </button>
         </div>
       ))}
+      <div>
+        <form onSubmit={addUser}>
+          <label>
+            Name:
+            <input 
+            name="name"
+            onChange={handleChanges}
+            value={formValues.name}
+            />
+          </label>
+          <label>
+            Bio:
+            <input 
+            name="bio"
+            onChange={handleChanges}
+            value={formValues.bio}
+            />
+          </label>
+          <button>{editing ? "Edit User" : "Add User"}</button>
+        </form>
+      </div>
     </div>
   );
 }
